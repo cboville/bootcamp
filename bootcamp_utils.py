5dbae8f193c59a79d43f8a39ab2579b305a833d5
Christina Boville <cboville@caltech.edu>
2017-06-21 19:18:57 -0700

    with ecdf function

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -1,6 +1,3 @@
-"""Bootcamp utilities with vaulable functions"""
-
-import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,22 +8,22 @@ colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
           '#bcbd22', '#17becf']
 sns.set(style='whitegrid', palette=colors, rc={'axes.labelsize': 16})
 
+def ecdf(data):
+    x=np.sort(data)
+    y=np.arange(1,len(data)+1)/len(data)
+    return x,y
+
 data=np.loadtxt('data/xa_high_food.csv', comments='#')
 ldata=np.loadtxt('data/xa_low_food.csv', comments='#')
 
-
-
-x=np.sort(data)
-y=np.arange(1,len(data)+1)/len(data)
-
-lx=np.sort(ldata)
-ly=np.arange(1,len(ldata)+1)/len(ldata)
+x_data, y_data=ecdf(data)
+x_ldata, y_ldata=ecdf(ldata)
 
 fig, ax=plt.subplots(1,1)
 _=ax.set_xlabel('eggs')
 _=ax.set_ylabel('probability')
 
-_=ax.plot(x,y, marker='.', linestyle='none')
-_=ax.plot(lx,ly, marker='.', linestyle='none')
+_=ax.plot(x_data, y_data, marker='.', linestyle='none')
+_=ax.plot(x_ldata, y_ldata, marker='.', linestyle='none')
 
 plt.show()
